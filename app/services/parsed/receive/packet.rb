6170ced1b83166ff83d3b911cefb98c4cc414601
Hamed Asghari <hasghari@gmail.com>
2014-09-16 14:06:50 -0700

    fix bug in getting constant

--- app/services/parsed/receive/packet.rb
+++ app/services/parsed/receive/packet.rb
@@ -3,7 +3,7 @@ module Parsed
     module Packet
       def self.create(header, payload)
         head = Header.new(header)
-        const_get(head.type).new(payload) if head.type
+        Parsed::Receive.const_get(head.type).new(payload) if head.type
       end
     end
   end
